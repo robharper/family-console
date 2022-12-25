8296555222a93c52c8b01daceb4eb3b36e8c8804
Robert Harper <robert.harper@gmail.com>
2022-12-25 14:03:24 -0500

    Proxy updated to match updates to lambda code

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,15 +4,15 @@ const { exchangeCodeForToken, refreshToken } = require('../lambda/tokenAuth');
 const jsonParser = bodyParser.json();
 
 module.exports = function(app) {
-  app.post('/token', jsonParser, (req, res) => {
-    exchangeCodeForToken(req.body).then(result => {
-      res.send(result);
-    });
-  });
-
-  app.post('/refresh', jsonParser, (req, res) => {
-    refreshToken(req.body).then(result => {
-      res.send(result);
-    });
+  app.post('/auth', jsonParser, (req, res) => {
+    if (req.body.action === 'code') {
+      exchangeCodeForToken(req.body).then(result => {
+        res.send(result);
+      });
+    } else if (req.body.action === 'refresh') {
+      refreshToken(req.body).then(result => {
+        res.send(result);
+      });
+    }
   });
 };
