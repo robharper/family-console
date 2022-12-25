1df94791af403561409cb369c5dff6b1495bd920
Robert Harper <robert.harper@gmail.com>
2022-12-25 14:01:11 -0500

    More resilient refresh tracking

--- src/components/photos/GooglePhotosSlideshow.tsx
+++ src/components/photos/GooglePhotosSlideshow.tsx
@@ -1,15 +1,15 @@
-import { useMemo } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { add, sub } from 'date-fns'
 import { useGoogleQuery } from "../../auth/googleAuthProvider";
 import { useToday } from "../../providers/todayProvider";
 import Loading from "../Loading";
-import { useTimeoutFn } from "react-use";
+import { useInterval, useTimeoutFn } from "react-use";
 import { MediaSearchResult, toGoogleDate } from "../../google/types";
 import { shuffle } from "../../util/fns";
 import Slideshow from "./Slideshow";
 import { useAppConfig } from "../../providers/appConfigProvider";
 
-const ONE_HOUR_MS = 60 * 60 * 1000;
+const TEN_SEC_MS = 10 * 1000;
 
 export default function GooglePhotosSlideshow() {
   const today = useToday();
@@ -17,7 +17,7 @@ export default function GooglePhotosSlideshow() {
 
   const photosSearchRequest = useMemo(() => {
     const dateStart = sub(today, {years: 1, months: 1});
-    const dateEnd = add(sub(today, {years: 1}), {months: 1})
+    const dateEnd = add(sub(today, {years: 1}), {months: 1});
 
     return {
       url: 'https://photoslibrary.googleapis.com/v1/mediaItems:search',
@@ -47,11 +47,22 @@ export default function GooglePhotosSlideshow() {
   // Search for a list of photos
   const { value: photosList, loading, retry: searchPhotos } = useGoogleQuery<MediaSearchResult>(photosSearchRequest);
 
-  const [, , resetPhotosSearch] = useTimeoutFn(() => {
-    // Re-query for list of google photos and reset timer for another hour out
-    searchPhotos();
-    resetPhotosSearch();
-  }, ONE_HOUR_MS);
+  // Track renewal of photos results (google photo results expire after 1 hour)
+  const [renewTime, setRenewTime] = useState<Date | undefined>();
+  useEffect(() => {
+    // When photos updated, renew the photos list in just under 1 hour
+    setRenewTime(add(new Date(), {minutes: 59}));
+  }, [photosList]);
+
+  useInterval(() => {
+    const now = new Date();
+    if (renewTime != null && now > renewTime) {
+      // Past our renew time, fetch new photos URLs
+      searchPhotos();
+    } else {
+      console.log('Still good!');
+    }
+  }, TEN_SEC_MS);
 
   const randomPhotosList = useMemo(() => photosList?.mediaItems ? shuffle(photosList.mediaItems) : [], [photosList])
 
